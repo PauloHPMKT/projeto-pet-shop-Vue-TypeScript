4764dbfcb0de26335f7949c5e3047129354b2690
Paulo Sergio <pauloserg861@gmail.com>
2022-08-15 21:53:00 -0300

    instalando biblioteca fontawesome

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+
 import App from './App.vue'
 import router from './router'
 
@@ -8,7 +13,9 @@ import './assets/main.css'
 
 const app = createApp(App)
 
+library.add(fas, far)
+
 app.use(createPinia())
 app.use(router)
-
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
