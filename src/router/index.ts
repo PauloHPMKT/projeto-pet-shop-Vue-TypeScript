f44525b6f22b15c1b6b69030d3355dbd61f9a004
Paulo Sergio <pauloserg861@gmail.com>
2022-08-15 21:07:58 -0300

    feature-routes criando rotas do projeto

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const About = '../views/About.vue'
+//separar rotas em um arquivo exportado
 const Home = '../views/Home.vue'
+const About = '../views/About.vue'
+const Service = '../views/Service.vue'
+const Gallery = '../views/Gallery.vue'
+const Contact = '../views/Contact.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +19,21 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: () => import(About),
+    },
+    {
+      path: '/services',
+      name: 'services',
+      component: () => import(Service),
+    },
+    {
+      path: '/gallery',
+      name: 'gallery',
+      component: () => import(Gallery),
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import(Contact),
     }
   ]
 })
